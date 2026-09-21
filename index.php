433f5f03fcd259aaefba890b1831ce8473f94aa4
Cory Sheppard <76483481+CorySheppard@users.noreply.github.com>
2026-09-21 20:07:03 +0100

    Added a UserController.php to handle collection/resource requests to users

--- index.php
+++ index.php
@@ -1,7 +1,24 @@
 <?php
 
+declare(strict_types=1);
 include './propelSetup.php';
+
+spl_autoload_register(function ($class) {
+    require __DIR__ . "/src/$class.php";
+});
+
+header("Content-type: application/json; charset=UTF-8");
 
 $parts = explode('/', $_SERVER['REQUEST_URI']);
 
-print_r($parts);
+// Writing this only for users currently, this will change!
+if ($parts[1] != "user") {
+    http_response_code(404);
+    exit;
+}
+
+$id = $parts[2] ?? null;
+
+$controller = new UserController;
+
+$controller->processRequest($_SERVER['REQUEST_METHOD'], $id);
